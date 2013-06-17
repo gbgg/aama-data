e19239024cae27c9b4e43e4ae7f236bc78ef03ae
Gene Gragg <g-gragg@uchicago.edu>
2013-06-17 19:25:08 +0200

    trying to have one-language branch

--- bin/fuseki.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# rev 12/22/12
-# Start the server before queries
-
- cd /cygdrive/c/Fuseki-0.2.4/
-./fuseki-server  --config=aamaconfig.ttl 
-#./fuseki-server -v  --update --loc=aama /aamaData
