32196a00c20f48b3b1ea0d9bd58d9aaa381ce360
Gene Gragg <g-gragg@uchicago.edu>
2014-03-21 10:19:03 -0500

    restored bin/generate-edn.sh, tested on kemant

--- bin/generate-edn.sh
+++ bin/generate-edn.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-# usage:  htmlgen.sh "dir"
+# usage: htmlgen.sh "dir"
 
 . bin/constants.sh
 
 echo "xml2edn" >> logs/xml2edn.log;
 
-#for d in `ls -d data`
-#do
-    # echo "$d ********************************************"
-	#fs=`find $d -name *xml`
-o#done
+for d in `ls -d data`
+do
+    echo "$d ********************************************"
+	fs=`find $d -name *xml`
+	#fs=`find $1 -name *xml`
+	for f in $fs
+	do
+		lang=${f%\.xml}
+		echo "generating ${f%-pdgms\.xml}-pdgms.edn from $f "
+		# set -x;
+		java -jar ${JARDIR}/${SAXON} \
+			-xi \
+			-s:$f \
+			-o:${f%-pdgms\.xml}-pdgms.edn \
+			-xsl:bin/xml2edn.xsl \
+			lang=`dirname ${f#data/}`;
+		# set +x;
+	done
+#done
 
