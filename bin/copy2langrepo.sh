3e76a44186df1ba40eb8f0654151a62aa7558f2d
Gene Gragg <g-gragg@uchicago.edu>
2014-03-27 18:09:10 -0500

    improved edn2ttl

--- bin/copy2langrepo.sh
+++ bin/copy2langrepo.sh
@@ -2,19 +2,20 @@
 # usage:  ~/aama-data/bin/copy2langrepo.sh
 
 # 03/21/14: 
+# 03/26/14: restricted to edn (xml now out of date)
 
 . bin/constants.sh
 
 for d in `ls -d data`
 do
 echo "$d ********************************************"
-fs=`find $d -name *xml`
-#fs=`find $1 -name *xml`
+fs=`find $d -name *edn`
+#fs=`find $1 -name *edn`
 	for f in $fs
 	do
-		lang=`basename ${f%-pdgms\.xml}`
+		lang=`basename ${f%-pdgms\.edn}`
 		echo copying files for $lang to aama/$lang
-		cp data/$lang/$lang-pdgms\.xml ../aama/$lang/
+		#cp data/$lang/$lang-pdgms\.xml ../aama/$lang/
 		cp data/$lang/$lang-pdgms\.edn ../aama/$lang/
 	done
 done
